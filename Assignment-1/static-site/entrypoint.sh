9d570bc04081db05ff9fe80d9e0776490c31ee1a
deepakkushwahax <deepakkushwaha7523@gmail.com>
2025-06-02 17:47:29 +0530

    assignment-1 added

---
+++ Assignment-1/static-site/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+HOUR=$(date +%H)
+
+if [ "$HOUR" -ge 10 ] && [ "$HOUR" -lt 12 ]; then
+  NAME="Mohan"
+elif [ "$HOUR" -ge 16 ] && [ "$HOUR" -lt 18 ]; then
+  NAME="Pankaj"
+else
+  NAME="-- Inactive Hours --"
+fi
+
+sed "s/{{name}}/$NAME/" /app/index.html.template > /usr/share/nginx/html/index.html
+
+# Start NGINX
+nginx -g 'daemon off;'
